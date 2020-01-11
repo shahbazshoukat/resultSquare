9569c237afbd0039e82b95a2ce02e895b7a5cf53
shahbaz shoukat <shahbazshoukat09@gmail.com>
2020-01-11 21:10:36 +0500

    package file updated

--- backend/constants/DBConstants.js
+++
@@ -1,8 +0,0 @@
-const DBConstants = Object.freeze({
-    MESSAGES: {
-        CONNECTED_TO_DATABASE: 'Connected to database',
-        DB_CONNECTION_FAILED: 'DB connection failed'
-    }
-});
-
-module.exports = DBConstants;
--- backend/enums/SectionEnums.js
+++
@@ -1,8 +0,0 @@
-const SectionEnums = Object.freeze({
-    Type: {
-        TEST: 0,
-        CLASS: 1
-    }
- });
- 
- module.exports = SectionEnums;
