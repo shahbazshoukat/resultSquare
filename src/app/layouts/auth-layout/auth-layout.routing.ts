9547ae24e2d367bbc9f8751fee4f966cd4b2589b
shahbaz shoukat <shahbazshoukat09@gmail.com>
2020-02-12 00:19:32 +0500

    new build

--- src/app/layouts/auth-layout/auth-layout.routing.ts
+++ src/app/layouts/auth-layout/auth-layout.routing.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from '../../guards/auth.guard';
 
 import { LoginComponent } from '../../pages/admin/login/login.component';
 import { RegisterComponent } from '../../pages/admin/register/register.component';
 
 export const AuthLayoutRoutes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'signup', component: RegisterComponent }
+    { path: 'signup', component: RegisterComponent, canActivate: [AuthGuard] }
 ];
