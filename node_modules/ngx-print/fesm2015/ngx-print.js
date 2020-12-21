c588505233334df2534beadcbf671a0c423a0304
shahbaz shoukat <shahbazshoukat09@gmail.com>
2020-12-21 21:38:46 +0500

    new-build

---
+++ node_modules/ngx-print/fesm2015/ngx-print.js
@@ -0,0 +1,178 @@
+import { Directive, HostListener, Input, NgModule } from '@angular/core';
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+class NgxPrintDirective {
+    constructor() {
+        this._printStyle = [];
+        /**
+         *
+         *
+         * \@memberof NgxPrintDirective
+         */
+        this.useExistingCss = false;
+        /**
+         *
+         *
+         * @return html for the given tag
+         *
+         * \@memberof NgxPrintDirective
+         */
+        this._styleSheetFile = '';
+    }
+    /**
+     *
+     *
+     * \@memberof NgxPrintDirective
+     * @param {?} values
+     * @return {?}
+     */
+    set printStyle(values) {
+        for (let key in values) {
+            if (values.hasOwnProperty(key)) {
+                this._printStyle.push((key + JSON.stringify(values[key])).replace(/['"]+/g, ''));
+            }
+        }
+        this.returnStyleValues();
+    }
+    /**
+     *
+     *
+     * \@memberof NgxPrintDirective
+     * @return {?} the string that create the stylesheet which will be injected
+     * later within <style></style> tag.
+     *
+     * -join/replace to transform an array objects to css-styled string
+     *
+     */
+    returnStyleValues() {
+        return `<style> ${this._printStyle.join(' ').replace(/,/g, ';')} </style>`;
+    }
+    /**
+     * \@memberof NgxPrintDirective
+     * @param {?} cssList
+     * @return {?}
+     */
+    set styleSheetFile(cssList) {
+        /** @type {?} */
+        let linkTagFn = (/**
+         * @param {?} cssFileName
+         * @return {?}
+         */
+        cssFileName => `<link rel="stylesheet" type="text/css" href="${cssFileName}">`);
+        if (cssList.indexOf(',') !== -1) {
+            /** @type {?} */
+            const valueArr = cssList.split(',');
+            for (let val of valueArr) {
+                this._styleSheetFile = this._styleSheetFile + linkTagFn(val);
+            }
+        }
+        else {
+            this._styleSheetFile = linkTagFn(cssList);
+        }
+    }
+    /**
+     * @private
+     * @return {?} string which contains the link tags containing the css which will
+     * be injected later within <head></head> tag.
+     *
+     */
+    returnStyleSheetLinkTags() {
+        return this._styleSheetFile;
+    }
+    /**
+     * @private
+     * @param {?} tag
+     * @return {?}
+     */
+    getElementTag(tag) {
+        /** @type {?} */
+        const html = [];
+        /** @type {?} */
+        const elements = document.getElementsByTagName(tag);
+        for (let index = 0; index < elements.length; index++) {
+            html.push(elements[index].outerHTML);
+        }
+        return html.join('\r\n');
+    }
+    /**
+     *
+     *
+     * \@memberof NgxPrintDirective
+     * @return {?}
+     */
+    print() {
+        /** @type {?} */
+        let printContents;
+        /** @type {?} */
+        let popupWin;
+        /** @type {?} */
+        let styles = '';
+        /** @type {?} */
+        let links = '';
+        if (this.useExistingCss) {
+            styles = this.getElementTag('style');
+            links = this.getElementTag('link');
+        }
+        printContents = document.getElementById(this.printSectionId).innerHTML;
+        popupWin = window.open("", "_blank", "top=0,left=0,height=auto,width=auto");
+        popupWin.document.open();
+        popupWin.document.write(`
+      <html>
+        <head>
+          <title>${this.printTitle ? this.printTitle : ""}</title>
+          ${this.returnStyleValues()}
+          ${this.returnStyleSheetLinkTags()}
+          ${styles}
+          ${links}
+        </head>
+        <body onload="window.print(); setTimeout(()=>{ window.close(); }, 0)">
+          ${printContents}
+        </body>
+      </html>`);
+        popupWin.document.close();
+    }
+}
+NgxPrintDirective.decorators = [
+    { type: Directive, args: [{
+                selector: "button[ngxPrint]"
+            },] }
+];
+NgxPrintDirective.propDecorators = {
+    printSectionId: [{ type: Input }],
+    printTitle: [{ type: Input }],
+    useExistingCss: [{ type: Input }],
+    printStyle: [{ type: Input }],
+    styleSheetFile: [{ type: Input }],
+    print: [{ type: HostListener, args: ['click',] }]
+};
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+class NgxPrintModule {
+}
+NgxPrintModule.decorators = [
+    { type: NgModule, args: [{
+                declarations: [NgxPrintDirective],
+                imports: [],
+                exports: [NgxPrintDirective]
+            },] }
+];
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+
+export { NgxPrintDirective, NgxPrintModule };
+
+//# sourceMappingURL=ngx-print.js.map
---
+++ node_modules/ngx-print/fesm5/ngx-print.js
@@ -0,0 +1,248 @@
+import { __values } from 'tslib';
+import { Directive, HostListener, Input, NgModule } from '@angular/core';
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+var NgxPrintDirective = /** @class */ (function () {
+    function NgxPrintDirective() {
+        this._printStyle = [];
+        /**
+         *
+         *
+         * \@memberof NgxPrintDirective
+         */
+        this.useExistingCss = false;
+        /**
+         *
+         *
+         * @return html for the given tag
+         *
+         * \@memberof NgxPrintDirective
+         */
+        this._styleSheetFile = '';
+    }
+    Object.defineProperty(NgxPrintDirective.prototype, "printStyle", {
+        /**
+         *
+         *
+         * @memberof NgxPrintDirective
+         */
+        set: /**
+         *
+         *
+         * \@memberof NgxPrintDirective
+         * @param {?} values
+         * @return {?}
+         */
+        function (values) {
+            for (var key in values) {
+                if (values.hasOwnProperty(key)) {
+                    this._printStyle.push((key + JSON.stringify(values[key])).replace(/['"]+/g, ''));
+                }
+            }
+            this.returnStyleValues();
+        },
+        enumerable: true,
+        configurable: true
+    });
+    /**
+     *
+     *
+     * @returns the string that create the stylesheet which will be injected
+     * later within <style></style> tag.
+     *
+     * -join/replace to transform an array objects to css-styled string
+     *
+     * @memberof NgxPrintDirective
+     */
+    /**
+     *
+     *
+     * \@memberof NgxPrintDirective
+     * @return {?} the string that create the stylesheet which will be injected
+     * later within <style></style> tag.
+     *
+     * -join/replace to transform an array objects to css-styled string
+     *
+     */
+    NgxPrintDirective.prototype.returnStyleValues = /**
+     *
+     *
+     * \@memberof NgxPrintDirective
+     * @return {?} the string that create the stylesheet which will be injected
+     * later within <style></style> tag.
+     *
+     * -join/replace to transform an array objects to css-styled string
+     *
+     */
+    function () {
+        return "<style> " + this._printStyle.join(' ').replace(/,/g, ';') + " </style>";
+    };
+    Object.defineProperty(NgxPrintDirective.prototype, "styleSheetFile", {
+        /**
+         * @memberof NgxPrintDirective
+         * @param cssList
+         */
+        set: /**
+         * \@memberof NgxPrintDirective
+         * @param {?} cssList
+         * @return {?}
+         */
+        function (cssList) {
+            var e_1, _a;
+            /** @type {?} */
+            var linkTagFn = (/**
+             * @param {?} cssFileName
+             * @return {?}
+             */
+            function (cssFileName) {
+                return "<link rel=\"stylesheet\" type=\"text/css\" href=\"" + cssFileName + "\">";
+            });
+            if (cssList.indexOf(',') !== -1) {
+                /** @type {?} */
+                var valueArr = cssList.split(',');
+                try {
+                    for (var valueArr_1 = __values(valueArr), valueArr_1_1 = valueArr_1.next(); !valueArr_1_1.done; valueArr_1_1 = valueArr_1.next()) {
+                        var val = valueArr_1_1.value;
+                        this._styleSheetFile = this._styleSheetFile + linkTagFn(val);
+                    }
+                }
+                catch (e_1_1) { e_1 = { error: e_1_1 }; }
+                finally {
+                    try {
+                        if (valueArr_1_1 && !valueArr_1_1.done && (_a = valueArr_1.return)) _a.call(valueArr_1);
+                    }
+                    finally { if (e_1) throw e_1.error; }
+                }
+            }
+            else {
+                this._styleSheetFile = linkTagFn(cssList);
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    /**
+     * @returns string which contains the link tags containing the css which will
+     * be injected later within <head></head> tag.
+     *
+     */
+    /**
+     * @private
+     * @return {?} string which contains the link tags containing the css which will
+     * be injected later within <head></head> tag.
+     *
+     */
+    NgxPrintDirective.prototype.returnStyleSheetLinkTags = /**
+     * @private
+     * @return {?} string which contains the link tags containing the css which will
+     * be injected later within <head></head> tag.
+     *
+     */
+    function () {
+        return this._styleSheetFile;
+    };
+    /**
+     * @private
+     * @param {?} tag
+     * @return {?}
+     */
+    NgxPrintDirective.prototype.getElementTag = /**
+     * @private
+     * @param {?} tag
+     * @return {?}
+     */
+    function (tag) {
+        /** @type {?} */
+        var html = [];
+        /** @type {?} */
+        var elements = document.getElementsByTagName(tag);
+        for (var index = 0; index < elements.length; index++) {
+            html.push(elements[index].outerHTML);
+        }
+        return html.join('\r\n');
+    };
+    /**
+     *
+     *
+     * @memberof NgxPrintDirective
+     */
+    /**
+     *
+     *
+     * \@memberof NgxPrintDirective
+     * @return {?}
+     */
+    NgxPrintDirective.prototype.print = /**
+     *
+     *
+     * \@memberof NgxPrintDirective
+     * @return {?}
+     */
+    function () {
+        /** @type {?} */
+        var printContents;
+        /** @type {?} */
+        var popupWin;
+        /** @type {?} */
+        var styles = '';
+        /** @type {?} */
+        var links = '';
+        if (this.useExistingCss) {
+            styles = this.getElementTag('style');
+            links = this.getElementTag('link');
+        }
+        printContents = document.getElementById(this.printSectionId).innerHTML;
+        popupWin = window.open("", "_blank", "top=0,left=0,height=auto,width=auto");
+        popupWin.document.open();
+        popupWin.document.write("\n      <html>\n        <head>\n          <title>" + (this.printTitle ? this.printTitle : "") + "</title>\n          " + this.returnStyleValues() + "\n          " + this.returnStyleSheetLinkTags() + "\n          " + styles + "\n          " + links + "\n        </head>\n        <body onload=\"window.print(); setTimeout(()=>{ window.close(); }, 0)\">\n          " + printContents + "\n        </body>\n      </html>");
+        popupWin.document.close();
+    };
+    NgxPrintDirective.decorators = [
+        { type: Directive, args: [{
+                    selector: "button[ngxPrint]"
+                },] }
+    ];
+    NgxPrintDirective.propDecorators = {
+        printSectionId: [{ type: Input }],
+        printTitle: [{ type: Input }],
+        useExistingCss: [{ type: Input }],
+        printStyle: [{ type: Input }],
+        styleSheetFile: [{ type: Input }],
+        print: [{ type: HostListener, args: ['click',] }]
+    };
+    return NgxPrintDirective;
+}());
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+var NgxPrintModule = /** @class */ (function () {
+    function NgxPrintModule() {
+    }
+    NgxPrintModule.decorators = [
+        { type: NgModule, args: [{
+                    declarations: [NgxPrintDirective],
+                    imports: [],
+                    exports: [NgxPrintDirective]
+                },] }
+    ];
+    return NgxPrintModule;
+}());
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+
+export { NgxPrintDirective, NgxPrintModule };
+
+//# sourceMappingURL=ngx-print.js.map
